82bb186e173b4b3eb2cbf464fb02fef7dc3c56f8
dimitrisKolovos <kolobosd98@gmail.com>
2025-04-05 22:06:08 +0300

    refactor: route point item composable.

---
+++ src/composables/RoutePointItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="['route-item', pointType]">
+        <p>{{ routePoint.location.address }}</p>
+        <p>{{ routePoint.location.friendlyAddress }}</p>
+        <p>{{ routePoint.type.toUpperCase() }}</p>
+        <p>{{ routePoint.status.toUpperCase() }}</p>
+    </div>
+
+</template>
+
+<script setup>
+
+defineProps({ 
+    routePoint: Object,
+    pointType: String
+ });
+
+</script>
+
+<style>
+
+.route-item{
+    padding: 8px;
+    border-bottom: 1px solid #bbb;
+}
+
+.route-item.service{
+    
+}
+
+.route-item.delivery{
+
+}
+
+.route-item.pickup{
+
+}
+
+.route-item.start{
+
+}
+
+.route-item.finish{
+
+}
+
+</style>
